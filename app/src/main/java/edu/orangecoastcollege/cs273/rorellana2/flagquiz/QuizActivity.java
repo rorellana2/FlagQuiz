93e9c66b58484c3d2cce455ebe00a65228264275
rorellana2 <romarioorellana949@gmail.com>
2016-09-22 16:34:11 -0700

    Starting on ActivityQuiz coding part

--- app/src/main/java/edu/orangecoastcollege/cs273/rorellana2/flagquiz/QuizActivity.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/rorellana2/flagquiz/QuizActivity.java
@@ -1,16 +1,29 @@
 package edu.orangecoastcollege.cs273.rorellana2.flagquiz;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class QuizActivity extends AppCompatActivity {
+    // Keys for reading data from SharedPreferences
+    public static final String CHOICES = "pref_numberOfChoices";
+    public static final String REGIONS = "pref_regionsToInclude";
 
+    private boolean phoneDevice = true; // used to force portrait mode
+    private boolean preferencesChanged = true;
+
+    /**
+     * onCreate generates the appropriate layout to inflate, depending on the
+     * screen size. If the device is large or x-large, it will load the content_main.xml
+     * (sv700dp-land) which includes both the fragment_quiz.xml and fragment_settings.xml
+     * Otherwise, it just inflates the standard content_main.xml with the fragment_quiz
+     *
+     * All default preferences are set using the preferences.xml file.
+     * @param savedInstanceState The saved state to restore (not being used)
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,14 +31,16 @@ public class QuizActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        // set default values in the app's SharedPreferences
+        PreferenceManager.setDefaultValues(this, R.xml.preferences, false);
+
+        // register listener for SharedPreferences changes
+        PreferenceManager.getDefaultSharedPreferences(this).
+                registerOnSharedPreferenceChangeListener(
+                        preferencesChangeListener);
+
+        // determine screen size (left off here)
+
     }
 
     @Override
